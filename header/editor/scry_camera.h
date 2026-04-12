fe0123b559cc96c5df100cb07d7329f8953bc88f
PandeManiac <Lee.Matthew.Carl@gmail.com>
2026-04-12 12:25:33 +0000

    Refactor camera structures and functions for improved clarity, consistency, and safety.

--- header/editor/scry_camera.h
+++ header/editor/scry_camera.h
@@ -3,14 +3,20 @@
 #include <cglm/cglm.h>
 #include <cglm/struct.h>
 
-typedef struct scry_camera2d
+typedef struct scry_viewport
+{
+	int width;
+	int height;
+} scry_viewport;
+
+typedef struct scry_camera
 {
 	vec2s center;
 	float zoom;
-} scry_camera2d;
+} scry_camera;
 
-void  scry_camera2d_init(scry_camera2d* camera);
-void  scry_camera2d_build_matrix(const scry_camera2d* camera, int viewport_width, int viewport_height, mat4 out_matrix);
-vec2s scry_camera2d_screen_to_world(const scry_camera2d* camera, float screen_x, float screen_y, int viewport_width, int viewport_height);
-void  scry_camera2d_zoom_at_screen(scry_camera2d* camera, float zoom_factor, float screen_x, float screen_y, int viewport_width, int viewport_height);
-void  scry_camera2d_pan_pixels(scry_camera2d* camera, float delta_screen_x, float delta_screen_y);
+void  scry_camera_init(scry_camera* camera);
+void  scry_camera_build_matrix(const scry_camera* camera, const scry_viewport* viewport, mat4 out_matrix);
+vec2s scry_camera_screen_to_world(const scry_camera* camera, vec2s screen_position, const scry_viewport* viewport);
+void  scry_camera_zoom_at_screen(scry_camera* camera, float zoom_factor, vec2s screen_position, const scry_viewport* viewport);
+void  scry_camera_pan_pixels(scry_camera* camera, vec2s delta_pixels);
--- src/editor/scry_camera.c
+++ src/editor/scry_camera.c
@@ -1,13 +1,12 @@
 #include "editor/scry_camera.h"
-
 #include "utils/scry_assert.h"
 
 #include <cglm/cglm.h>
 
 static float scry_camera_safe_zoom(float zoom);
-static void	 scry_camera_validate_viewport(int viewport_width, int viewport_height);
+static void	 scry_camera_validate_viewport(const scry_viewport* viewport);
 
-void scry_camera2d_init(scry_camera2d* camera)
+void scry_camera_init(scry_camera* camera)
 {
 	ASSERT_FATAL(camera);
 
@@ -15,60 +14,67 @@ void scry_camera2d_init(scry_camera2d* camera)
 	camera->zoom   = 1.0f;
 }
 
-void scry_camera2d_build_matrix(const scry_camera2d* camera, int viewport_width, int viewport_height, mat4 out_matrix)
+void scry_camera_build_matrix(const scry_camera* camera, const scry_viewport* viewport, mat4 out_matrix)
 {
+	ASSERT_FATAL(camera);
+	ASSERT_FATAL(viewport);
+	ASSERT_FATAL(out_matrix);
+
+	scry_camera_validate_viewport(viewport);
+
 	const float zoom		= scry_camera_safe_zoom(camera->zoom);
-	const float half_width	= (float)viewport_width / (2.0f * zoom);
-	const float half_height = (float)viewport_height / (2.0f * zoom);
+	const float half_width	= (float)viewport->width / (2.0f * zoom);
+	const float half_height = (float)viewport->height / (2.0f * zoom);
 	const float left		= camera->center.x - half_width;
 	const float right		= camera->center.x + half_width;
 	const float top			= camera->center.y - half_height;
 	const float bottom		= camera->center.y + half_height;
 
-	ASSERT_FATAL(camera);
-	ASSERT_FATAL(out_matrix);
-	scry_camera_validate_viewport(viewport_width, viewport_height);
-
 	glm_ortho(left, right, bottom, top, -1.0f, 1.0f, out_matrix);
 }
 
-vec2s scry_camera2d_screen_to_world(const scry_camera2d* camera, float screen_x, float screen_y, int viewport_width, int viewport_height)
+vec2s scry_camera_screen_to_world(const scry_camera* camera, vec2s screen_position, const scry_viewport* viewport)
 {
+	ASSERT_FATAL(camera);
+	ASSERT_FATAL(viewport);
+
+	scry_camera_validate_viewport(viewport);
+
 	const float zoom  = scry_camera_safe_zoom(camera->zoom);
 	vec2s		world = { { 0.0f, 0.0f } };
 
-	ASSERT_FATAL(camera);
-	scry_camera_validate_viewport(viewport_width, viewport_height);
+	world.x = camera->center.x + ((screen_position.x - ((float)viewport->width * 0.5f)) / zoom);
+	world.y = camera->center.y + ((screen_position.y - ((float)viewport->height * 0.5f)) / zoom);
 
-	world.x = camera->center.x + ((screen_x - ((float)viewport_width * 0.5f)) / zoom);
-	world.y = camera->center.y + ((screen_y - ((float)viewport_height * 0.5f)) / zoom);
 	return world;
 }
 
-void scry_camera2d_zoom_at_screen(scry_camera2d* camera, float zoom_factor, float screen_x, float screen_y, int viewport_width, int viewport_height)
+void scry_camera_zoom_at_screen(scry_camera* camera, float zoom_factor, vec2s screen_position, const scry_viewport* viewport)
 {
-	const vec2s world_before = scry_camera2d_screen_to_world(camera, screen_x, screen_y, viewport_width, viewport_height);
+	ASSERT_FATAL(camera);
+	ASSERT_FATAL(viewport);
+	ASSERT_FATAL(zoom_factor > 0.0f);
+
+	scry_camera_validate_viewport(viewport);
+
+	const vec2s world_before = scry_camera_screen_to_world(camera, screen_position, viewport);
 	vec2s		world_after	 = { { 0.0f, 0.0f } };
 
-	ASSERT_FATAL(camera);
-	ASSERT_FATAL(zoom_factor > 0.0f);
-	scry_camera_validate_viewport(viewport_width, viewport_height);
-
 	camera->zoom = scry_camera_safe_zoom(camera->zoom * zoom_factor);
-	world_after	 = scry_camera2d_screen_to_world(camera, screen_x, screen_y, viewport_width, viewport_height);
+	world_after	 = scry_camera_screen_to_world(camera, screen_position, viewport);
 
 	camera->center.x += world_before.x - world_after.x;
 	camera->center.y += world_before.y - world_after.y;
 }
 
-void scry_camera2d_pan_pixels(scry_camera2d* camera, float delta_screen_x, float delta_screen_y)
+void scry_camera_pan_pixels(scry_camera* camera, vec2s delta_pixels)
 {
-	const float zoom = scry_camera_safe_zoom(camera->zoom);
-
 	ASSERT_FATAL(camera);
 
-	camera->center.x -= delta_screen_x / zoom;
-	camera->center.y -= delta_screen_y / zoom;
+	const float zoom = scry_camera_safe_zoom(camera->zoom);
+
+	camera->center.x -= delta_pixels.x / zoom;
+	camera->center.y -= delta_pixels.y / zoom;
 }
 
 static float scry_camera_safe_zoom(float zoom)
@@ -76,8 +82,9 @@ static float scry_camera_safe_zoom(float zoom)
 	return zoom > 0.0f ? zoom : 1.0f;
 }
 
-static void scry_camera_validate_viewport(int viewport_width, int viewport_height)
+static void scry_camera_validate_viewport(const scry_viewport* viewport)
 {
-	ASSERT_FATAL(viewport_width > 0);
-	ASSERT_FATAL(viewport_height > 0);
+	ASSERT_FATAL(viewport);
+	ASSERT_FATAL(viewport->width > 0);
+	ASSERT_FATAL(viewport->height > 0);
 }
